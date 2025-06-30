994f96591f397a5ba1af22968c9ff4bc58593a3e
Saurabh A <saurabhanand808@gmail.com>
2025-06-30 21:51:40 +0100

    Update Navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/Navbar.css';
+import '../styles/Navbar.css'; // Just import CSS here, don't paste it in this file
 
 const Navbar = () => {
   return (
     <nav className="navbar" role="navigation" aria-label="Main Navigation">
       <h1 className="navbar__title">NASA Explorer</h1>
-      <ul className="navbar__list">
-        <li><NavLink to="/" className="navbar__link" activeClassName="navbar__link--active" exact>Home</NavLink></li>
-        <li><NavLink to="/apod" className="navbar__link" activeClassName="navbar__link--active">APOD</NavLink></li>
-        <li><NavLink to="/mars" className="navbar__link" activeClassName="navbar__link--active">Mars Rover</NavLink></li>
-        <li><NavLink to="/neo" className="navbar__link" activeClassName="navbar__link--active">NEO</NavLink></li>
-        <li><NavLink to="/about" className="navbar__link" activeClassName="navbar__link--active">About</NavLink></li>
+
+      <ul className="navbar__links">
+        <li>
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              isActive ? "navbar__link navbar__link--active" : "navbar__link"
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/apod"
+            className={({ isActive }) =>
+              isActive ? "navbar__link navbar__link--active" : "navbar__link"
+            }
+          >
+            APOD
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/mars"
+            className={({ isActive }) =>
+              isActive ? "navbar__link navbar__link--active" : "navbar__link"
+            }
+          >
+            Mars Rover
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/neo"
+            className={({ isActive }) =>
+              isActive ? "navbar__link navbar__link--active" : "navbar__link"
+            }
+          >
+            NEO
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "navbar__link navbar__link--active" : "navbar__link"
+            }
+          >
+            About
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
